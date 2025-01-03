efd1b9f1965dfe6bf7ce4a091e9359d71115f3b4
李子耀 <6733170360@qq.com>
2025-01-03 12:49:09 +0800

    feat：优化sql

--- src/main/java/com/lizy/rewritenew/controller/ExcelController.java
+++ src/main/java/com/lizy/rewritenew/controller/ExcelController.java
@@ -94,15 +94,11 @@ public class ExcelController {
                     .map(OriginSimple::getUniqueKey)
                     .collect(Collectors.toSet());
 
-            List<Employee> notInSimplesList = employeeList.stream()
-                    .filter(employee -> !simpleUniqueKeys.contains(employee.getUniqueKey()))
-                    .collect(Collectors.toList());
-
             HashMap<String, String> positionNameMap = new HashMap<>();
             HashMap<String, String> schoolNameMap = new HashMap<>();
 
             HashMap<String, String> remainPositionNameMap = new HashMap<>();
-            HashMap<String, String> remainSchoolNameMap = new HashMap<>();
+            TreeMap<String, String> remainSchoolNameMap = new TreeMap<>();
             List<OriginSimple> updateList = new ArrayList<>();
             List<OriginSimple> noInEmployeeList = new ArrayList<>();
             for (OriginSimple simple : simples) {
@@ -118,13 +114,18 @@ public class ExcelController {
                 if (StrUtil.startWith(simple.getSrzwmc(),"d")) {
                     positionNameMap.put(simple.getSrzwmc(),employee.getPositionTitle());
                 }
-                //存进去的 第一学校
-                if (StrUtil.startWith(simple.getHighestSchool(),"d")) {
-                    schoolNameMap.put(simple.getHighestSchool(),employee.getFirstSchool());
+//                //存进去的 第一学校
+                if (StrUtil.startWith(simple.getFirstSchool(),"d")) {
+                    schoolNameMap.put(simple.getFirstSchool(),employee.getFirstSchool());
                 }
+
                 //存进去的 最高学校
+                if (StrUtil.startWith(simple.getHighestSchool(),"d")) {
+                    schoolNameMap.put(simple.getHighestSchool(),employee.getHighestSchool());
+                }
+//                //存进去的 第一学校
 //                if (StrUtil.startWith(simple.getHighestSchool(),"d")) {
-//                    schoolNameMap.put(simple.getHighestSchool(),employee.getHighestSchool());
+//                    schoolNameMap.put(simple.getFirstSchool(),employee.getFirstSchool());
 //                }
 //                if (StrUtil.startWith(simple.getSrzwmc(),"d")) {
 //                    positionNameMap.put(simple.getSrzwmc(),simple.getXm()+":"+employee.getPositionTitle());
@@ -159,7 +160,137 @@ public class ExcelController {
                     remainSchoolNameMap.put(x.getName(),x.getValue());
                 }
             });
+            String jjj = JSON.toJSONString(remainSchoolNameMap);
+            log.info("还有{}条职务加密数据还没有解决",remainPositionNameMap.size());
+            log.info("还有{}条学校加密数据还没有解决",remainSchoolNameMap.size());
+//            this.updateThis(updateList);
+            log.info("更新数据，共更新{}条数据",updateList.size());
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "读取文件失败：" + e.getMessage();
+        }
 
+    }
+
+    /**
+     * 针对不同县加解密 不同的情况
+     * @param file 上传的Excel文件
+     * @return 操作结果
+     */
+    @PostMapping("/printInfo")
+    @Deprecated
+    public String printInfo(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "文件为空，请选择文件";
+        }
+        try {
+            // 读取Excel文件
+            EmployeeDataListener listener = new EmployeeDataListener();
+            EasyExcel.read(file.getInputStream(), Employee.class, listener)
+                    .sheet().headRowNumber(2).doRead();
+
+            // 获取解析后的数据列表
+            List<Employee> employeeList = listener.getEmployeeList();
+            log.info("文件读取成功，共解析到{}条数据",employeeList.size());
+            // 这里可以进行其他操作，例如保存到数据库
+            // 用于存储重复的Employee对象
+            List<Employee> duplicateEmployees = new ArrayList<>();
+
+            // 使用Stream API来检测和收集重复的Employee对象
+            Map<String, Long> employeeCountMap = employeeList.stream()
+                    .collect(Collectors.groupingBy(
+                            Employee::getUniqueKey,
+                            Collectors.counting() // 计算每个组的大小
+                    ));
+
+            // 找出重复的Employee对象
+            employeeCountMap.forEach((key, count) -> {
+                if (count > 1) { // 如果某个key出现超过一次，则认为是重复的
+                    // 从原始列表中找出所有具有该key的Employee对象
+                    List<Employee> duplicates = employeeList.stream()
+                            .filter(e -> (e.getUniqueKey()).equals(key))
+                            .collect(Collectors.toList());
+                    duplicateEmployees.addAll(duplicates); // 将重复的Employee对象添加到列表中
+                    System.out.println("Found duplicates for key: " + key + ", Employees: " + duplicates);
+                }
+            });
+
+            employeeList.removeAll(duplicateEmployees);
+
+            Map<String, Employee> employeeMap = employeeList.stream().collect(Collectors.toMap(Employee::getUniqueKey, Function.identity()));
+            log.info("转换map的keys数量：{}",employeeMap.keySet().size());
+            //获取excel
+            List<OriginSimple> simples = gzRyJbxxService.listSimple();
+            // 用于存储不在simples列表中的Employee数据
+
+            // 使用集合存储simples中的uniqueKey
+            Set<String> simpleUniqueKeys = simples.stream()
+                    .map(OriginSimple::getUniqueKey)
+                    .collect(Collectors.toSet());
+
+            List<Employee> notInSimplesList = employeeList.stream()
+                    .filter(employee -> !simpleUniqueKeys.contains(employee.getUniqueKey()))
+                    .collect(Collectors.toList());
+
+            HashMap<String, String> positionNameMap = new HashMap<>();
+            HashMap<String, String> schoolNameMap = new LinkedHashMap<>();
+
+            HashMap<String, String> remainPositionNameMap = new HashMap<>();
+            TreeMap<String, String> remainSchoolNameMap = new TreeMap<>();
+            List<OriginSimple> updateList = new ArrayList<>();
+            List<OriginSimple> noInEmployeeList = new ArrayList<>();
+            for (OriginSimple simple : simples) {
+                String uniqueKey = simple.getUniqueKey();
+                Employee employee = employeeMap.get(uniqueKey);
+                if (employee == null) {
+                    log.info("失败的key{}",uniqueKey);
+                    noInEmployeeList.add(simple);
+                    continue;
+                }
+                simple.setSfzh(employee.getIdNumber());
+                simple.setXm(employee.getName());
+
+                if (StrUtil.startWith(simple.getSrzwmc(),"d")) {
+                    positionNameMap.put(simple.getSrzwmc(),simple.getXm()+":"+employee.getPositionTitle());
+                }
+//                //存进去的 第一学校
+                if (StrUtil.startWith(simple.getFirstSchool(),"d")) {
+                    schoolNameMap.put(simple.getFirstSchool(),simple.getXm()+":"+employee.getFirstSchool());
+                }
+//                //存进去的 第一学校
+//                if (StrUtil.startWith(simple.getHighestSchool(),"d")) {
+//                    schoolNameMap.put(simple.getHighestSchool(),simple.getXm()+":"+employee.getFirstSchool());
+//                }
+//
+//
+                //存进去的 最高学校
+                if (StrUtil.startWith(simple.getHighestSchool(),"d")) {
+                    schoolNameMap.put(simple.getHighestSchool(),simple.getXm()+":"+employee.getHighestSchool());
+                }
+                updateList.add(simple);
+            }
+            String jsonString = JSON.toJSONString(positionNameMap);
+            String jsonString2 = JSON.toJSONString(schoolNameMap);
+            //获取所有职务加密
+            List<NameValueDTO> allCryptPositionName = gzRyJbxxService.listAllPositionName();
+            //获取所有学校加密
+            List<NameValueDTO> allCryptSchoolName = gzRyJbxxService.listAllSchoolName();
+//            //有多少map
+//            List<String> remainPositionList = new ArrayList<>();
+//            List<String> remainSchoolList = new ArrayList<>();
+            allCryptPositionName.forEach(x->{
+                if(!positionNameMap.containsKey(x.getValue())){
+                    remainPositionNameMap.put(x.getName(),x.getValue());
+                }
+            });
+
+            allCryptSchoolName.forEach(x->{
+                if(!schoolNameMap.containsKey(x.getValue())){
+                    remainSchoolNameMap.put(x.getName(),x.getValue());
+                }
+            });
+            String jjj = JSON.toJSONString(remainSchoolNameMap);
             log.info("还有{}条职务加密数据还没有解决",remainPositionNameMap.size());
             log.info("还有{}条学校加密数据还没有解决",remainSchoolNameMap.size());
 //            this.updateThis(updateList);
